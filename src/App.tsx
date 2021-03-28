08780c67133e2055ca43ac63ac93c2a9f5901402
namdh3012 <49227995+namdh3012@users.noreply.github.com>
2021-03-28 22:16:57 +0700

    update title

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import TodoList from "./component/todo-list/todo-list";
 import { TodoEntity } from "./entities/todo-entity";
 import { selectTodoList } from "./features/todo-slice";
 
+const titleNewTask = "New Task";
+const titleTodoList = "To Do List";
+
 const App = () => {
   const todoList = useSelector(selectTodoList);
 
@@ -22,11 +25,13 @@ const App = () => {
     <div className={styles.app}>
       <div className={styles.appHeader}>
         <div className={styles.createTaskArea}>
+          <div className={styles.title}>{titleNewTask}</div>
           <div className={styles.alignCenter}>
             <CreateEditTodo />
           </div>
         </div>
         <div className={styles.taskListArea}>
+          <div className={styles.title}>{titleTodoList}</div>
           <TodoList />
           {isShowPopup && <BulkAction />}
         </div>
